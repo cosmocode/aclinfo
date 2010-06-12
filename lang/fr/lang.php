e2a2557ca24d71ae7d54ea2f9737452379619a39
Andreas Gohr <andi@splitbrain.org>
2010-06-12 17:12:15 +0200

    French translation added

---
+++ lang/fr/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['perm0'] = '%s ne peut pas lire cette page';
+$lang['perm1'] = '%s peut lire cette page';
+$lang['perm2'] = '%s peut éditer cette page';
+$lang['perm4'] = '%s peut créer une page';
+$lang['perm8'] = '%s peut charger un fichier';
+$lang['perm16'] = '%s peut effacer cette page';
