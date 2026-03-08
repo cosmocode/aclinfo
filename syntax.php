8769b165848b15463685dac63605df954c377d06
Andreas Gohr <andi@splitbrain.org>
2026-03-08 15:09:28 +0100

    code cleanup fixes #8

--- syntax.php
+++ syntax.php
@@ -1,77 +1,62 @@
 <?php
+
+use dokuwiki\Extension\SyntaxPlugin;
+
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Andreas Gohr <andi@splitbrain.org>
  */
-// must be run within Dokuwiki
-if(!defined('DOKU_INC')) die();
-
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'syntax.php');
-
-/**
- * All DokuWiki plugins to extend the parser/rendering mechanism
- * need to inherit from this class
- */
-class syntax_plugin_aclinfo extends DokuWiki_Syntax_Plugin {
-
-    /**
-     * What kind of syntax are we?
-     */
-    function getType(){
+class syntax_plugin_aclinfo extends SyntaxPlugin
+{
+    /** @inheritdoc */
+    public function getType()
+    {
         return 'substition';
     }
 
-    /**
-     * What about paragraphs?
-     */
-    function getPType(){
+    /** @inheritdoc */
+    public function getPType()
+    {
         return 'block';
     }
 
-    /**
-     * Where to sort in?
-     */
-    function getSort(){
+    /** @inheritdoc */
+    public function getSort()
+    {
         return 155;
     }
 
-
-    /**
-     * Connect pattern to lexer
-     */
-    function connectTo($mode) {
-        $this->Lexer->addSpecialPattern('~~ACLINFO!?[^~]*?~~',$mode,'plugin_aclinfo');
+    /** @inheritdoc */
+    public function connectTo($mode)
+    {
+        $this->Lexer->addSpecialPattern('~~ACLINFO!?[^~]*?~~', $mode, 'plugin_aclinfo');
     }
 
-
-    /**
-     * Handle the match
-     */
-    function handle($match, $state, $pos, Doku_Handler $handler){
-        $match = substr($match,10,-2);
-        return array($match);
+    /** @inheritdoc */
+    public function handle($match, $state, $pos, Doku_Handler $handler)
+    {
+        $match = substr($match, 10, -2);
+        return [$match];
     }
 
-    /**
-     * Create output
-     */
-    function render($format, Doku_Renderer $R, $data) {
+    /** @inheritdoc */
+    public function render($format, Doku_Renderer $R, $data)
+    {
         global $INFO;
-        if($format != 'xhtml') return false;
+        if ($format != 'xhtml') return false;
 
-        if(!$data[0]) {
+        if (!$data[0]) {
             $page = $INFO['id'];
         } else {
             $page = $data[0];
         }
 
-        $perms = $this->_aclcheck($page);
+        $perms = $this->aclCheck($page);
         $R->listu_open();
-        foreach((array)$perms as $who => $p){
+        foreach ((array)$perms as $who => $p) {
             $R->listitem_open(1);
             $R->listcontent_open();
-            $R->cdata(sprintf($this->getLang('perm'.$p), urldecode($who)));
+            $R->cdata(sprintf($this->getLang('perm' . $p), urldecode($who)));
             $R->listcontent_close();
             $R->listitem_close();
         }
@@ -79,58 +64,62 @@ class syntax_plugin_aclinfo extends DokuWiki_Syntax_Plugin {
         return true;
     }
 
-    function _aclcheck($id){
-        global $conf;
+    /**
+     * Parse the ACL setup and return the permissions for the given page ID.
+     *
+     * @param string $id The page ID to check
+     * @return array
+     */
+    protected function aclCheck($id)
+    {
         global $AUTH_ACL;
 
         $id    = cleanID($id);
         $ns    = getNS($id);
-        $perms = array();
+        $perms = [];
 
         //check exact match first
-        $matches = preg_grep('/^'.preg_quote($id,'/').'\s+/',$AUTH_ACL);
-        if(count($matches)){
-            foreach($matches as $match){
-                $match = preg_replace('/#.*$/','',$match); //ignore comments
-                $acl   = preg_split('/\s+/',$match);
-                if($acl[2] > AUTH_DELETE) $acl[2] = AUTH_DELETE; //no admins in the ACL!
-                if(!isset($perms[$acl[1]])) $perms[$acl[1]] = $acl[2];
+        $matches = preg_grep('/^' . preg_quote($id, '/') . '\s+/', $AUTH_ACL);
+        if (count($matches)) {
+            foreach ($matches as $match) {
+                $match = preg_replace('/#.*$/', '', $match); //ignore comments
+                $acl   = preg_split('/\s+/', $match);
+                if ($acl[2] > AUTH_DELETE) $acl[2] = AUTH_DELETE; //no admins in the ACL!
+                if (!isset($perms[$acl[1]])) $perms[$acl[1]] = $acl[2];
             }
         }
 
         //still here? do the namespace checks
-        if($ns){
-            $path = $ns.':\*';
-        }else{
+        if ($ns) {
+            $path = $ns . ':\*';
+        } else {
             $path = '\*'; //root document
         }
 
-        do{
-            $matches = preg_grep('/^'.$path.'\s+/',$AUTH_ACL);
-            if(count($matches)){
-                foreach($matches as $match){
-                    $match = preg_replace('/#.*$/','',$match); //ignore comments
-                    $acl   = preg_split('/\s+/',$match);
-                    if($acl[2] > AUTH_DELETE) $acl[2] = AUTH_DELETE; //no admins in the ACL!
-                    if(!isset($perms[$acl[1]])) $perms[$acl[1]] = $acl[2];
+        do {
+            $matches = preg_grep('/^' . $path . '\s+/', $AUTH_ACL);
+            if (count($matches)) {
+                foreach ($matches as $match) {
+                    $match = preg_replace('/#.*$/', '', $match); //ignore comments
+                    $acl   = preg_split('/\s+/', $match);
+                    if ($acl[2] > AUTH_DELETE) $acl[2] = AUTH_DELETE; //no admins in the ACL!
+                    if (!isset($perms[$acl[1]])) $perms[$acl[1]] = $acl[2];
                 }
             }
 
             //get next higher namespace
             $ns   = getNS($ns);
 
-            if($path != '\*'){
-                $path = $ns.':\*';
-                if($path == ':\*') $path = '\*';
-            }else{
+            if ($path != '\*') {
+                $path = $ns . ':\*';
+                if ($path == ':\*') $path = '\*';
+            } else {
                 //we did this already
                 //break here
                 break;
             }
-        }while(1); //this should never loop endless
+        } while (1); //this should never loop endless
 
         return $perms;
     }
 }
-
-//Setup VIM: ex: et ts=4 enc=utf-8 :
